ca24d2335fa5b33bf9b9937411341d2cca2d7b91
Exist2Resist <exist2resist@outlook.com>
2018-12-23 22:55:44 -0700

    Update Flash_Backup.sh

--- exist2resist/Flash_Backup.sh
+++ exist2resist/Flash_Backup.sh
@@ -1,11 +1,10 @@
-SOURCE=/boot/*
-
-DESTINATION=/mnt/disks/DataDisk/backups/flash/
-
+SOURCE="/boot/*"
+DESTINATION="/mnt/disks/DataDisk/backups/flash/"
+FILENAME="FLASH_`date +%F_%H-%M-%S`.tar"
 DAYS=14
 
-tar -cf $SOURCE $DESTINATION
+tar -cf $SOURCE $DESTINATION$FILENAME
 
 cd $DESTINATION
 
-nice find . -mtime +$DAYS -exec rm {} \; 
+nice find . -mtime +$DAYS -exec rm {} \;
