b81b48fb6d2cbdf6f18b10f6bc5fb8b3717325c2
Exist2Resist <exist2resist@outlook.com>
2021-02-15 15:08:34 -0700

    Update maria_create_user.sh

--- exist2resist/scripts/maria_create_user.sh
+++ exist2resist/scripts/maria_create_user.sh
@@ -1,3 +1,3 @@
 CREATE USER admin IDENTIFIED BY 'somesecret';
 GRANT USAGE ON *.* TO nextcloud@'%' IDENTIFIED BY 'somesecret';
-GRANT ALL PRIVILEGES ON nextcloud.* TO 'nx_admin'@'%' identified BY 'somesecret';
+GRANT ALL PRIVILEGES ON nextcloud.* TO admin19@'%' identified BY 'somesecret';
