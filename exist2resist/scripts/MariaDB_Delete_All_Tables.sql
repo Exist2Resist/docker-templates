76dadad794f7b23aef1048b5f7d88dfece956d8d
Exist2Resist <exist2resist@outlook.com>
2021-02-15 15:26:09 -0700

    Update MariaDB_Delete_All_Tables.sql

--- exist2resist/scripts/MariaDB_Delete_All_Tables.sql
+++ exist2resist/scripts/MariaDB_Delete_All_Tables.sql
@@ -1,9 +1,10 @@
+SET @database = 'nextcloud';
 SET FOREIGN_KEY_CHECKS = 0;
 SET GROUP_CONCAT_MAX_LEN=32768;
 SET @tables = NULL;
 SELECT GROUP_CONCAT('`', table_name, '`') INTO @tables
   FROM information_schema.tables
-  WHERE table_schema = 'nextcloud';
+  WHERE table_schema = @database;
 SELECT IFNULL(@tables,'dummy') INTO @tables;
 SET @tables = CONCAT('DROP TABLE IF EXISTS ', @tables);
 PREPARE stmt FROM @tables;
